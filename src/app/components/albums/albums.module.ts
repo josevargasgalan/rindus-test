9986f3834c1d2fd0badc2c4d068e0d9799c44a3f
Jose <Jose.Vargas@payever.org>
2022-02-03 18:36:25 +0100

    Add album component

--- src/app/components/albums/albums.module.ts
+++ src/app/components/albums/albums.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AlbumsComponent } from './albums.component';
+import { AlbumComponent } from './components/album/album.component';
 
 @NgModule({
-  declarations: [AlbumsComponent],
+  declarations: [AlbumsComponent, AlbumComponent],
   imports: [CommonModule],
   exports: [AlbumsComponent]
 })
